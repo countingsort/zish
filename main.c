e9a45150ef9fefbff5614ad6c43c4ddcd86e44b3
meyni <niclas@niclasmeyer.berlin>
2018-04-02 12:45:13 +0200

    Now printing kawaii smileys

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-void zish_loop(void);
+#define NUM_KAWAII_SMILEYS 5
+static char *kawaii_smileys[5] = {
+    "(▰˘◡˘▰)",
+    "♥‿♥",
+    "(✿ ♥‿♥)",
+    ".ʕʘ‿ʘʔ.",
+    "≧◡≦"
+};
+
+enum status_code {
+    STAT_NORMAL,
+    STAT_EXIT
+};
+
+static void zish_loop(void);
 
 int main(void)
 {
     // Load stuff
+    srand(time(NULL));
 
     // REPL
     zish_loop();
@@ -15,10 +31,19 @@ int main(void)
     return EXIT_SUCCESS;
 }
 
-void zish_loop(void)
+static void zish_loop(void)
 {
-    char *line = NULL;
+    char  *line = NULL;
     char **args = NULL;
-    int status;
+    enum status_code status = STAT_NORMAL;
+
+    do {
+        int kawaii_smiley_index = rand() % NUM_KAWAII_SMILEYS;
+        printf(
+            "\033[38;5;057mAwaiting your command, senpai. \033[38;5;197m%s \033[38;5;255m",
+            kawaii_smileys[kawaii_smiley_index]
+        );
+        getc(stdin);
+    } while (status != STAT_EXIT);
 }
 
