dff5369f4f428139a07397900bc83bb4fd3cbf2a
meyni <niclas@niclasmeyer.berlin>
2018-04-20 13:49:25 +0200

    Var above functions

--- aliases.h
+++ aliases.h
@@ -10,15 +10,15 @@ struct alias {
 };
 
 /**
+ * NULL terminated list of aliases
+ */
+extern struct alias **aliases = NULL;
+
+/**
  * Builtin: defines an alias
  *
  * @returns status of the command
  */
 extern enum status_code zish_define_alias(int argc, char **argv);
-
-/**
- * NULL terminated list of aliases
- */
-extern struct alias **aliases = NULL;
 
 #endif /* ALIASES H */
