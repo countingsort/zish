4177f09d79e894d858203ea149f4e08018a24a27
meyni <niclas@niclasmeyer.berlin>
2018-04-22 18:43:11 +0200

    Manually merged develop into master

---
+++ interrupt_handler.h
@@ -0,0 +1,10 @@
+#ifndef INTERRUPT_HANDLER_H
+#define INTERRUPT_HANDLER_H
+
+/**
+* Registers the SIGINT interrupt handler
+*/
+void zish_register_interrupt_handler(void);
+
+#endif /* INTERRUPT_HANDLER_H */
+
